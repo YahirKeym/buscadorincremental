ff3078027cadd39468b0bd36dcccbe138ddc1ec6
YahirKeym <arzkaner@gmail.com>
2019-06-20 11:55:15 -0500

    Se hace la implementación de la ruta de java script a buscar y del input que manejara los datos

--- index.php
+++ index.php
@@ -5,13 +5,15 @@
 	<title>Buscador Incremental</title>
 </head>
 <body>
+	<input type="search" name="busquedaimplacable">
 	<ul>
-		<li data-search=""></li>
-		<li data-search=""></li>
-		<li data-search=""></li>
-		<li data-search=""></li>
-		<li data-search=""></li>
-		<li data-search=""></li>
+		<li data-search="Soy Yahir">Soy Yahir</li>
+		<li data-search="No Soy Yahir">No Soy Yahir</li>
+		<li data-search="Buscar">Buscar</li>
+		<li data-search="Manzana">Manzana</li>
+		<li data-search="Pera">Pera</li>
+		<li data-search="Alabahama">Alabahama</li>
 	</ul>
+	<script type="text/javascript" src="assets/js/buscador.js"></script>
 </body>
 </html>
