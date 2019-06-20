c939bef72deaf5bc05755d012ebeacac13be4fa6
YahirKeym <arzkaner@gmail.com>
2019-06-20 11:06:24 -0500

    Se agrega el diagrama de como se espera la representación del programa, y la estructura inicial del index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Buscador Incremental</title>
+</head>
+<body>
+	<ul>
+		<li data-search=""></li>
+		<li data-search=""></li>
+		<li data-search=""></li>
+		<li data-search=""></li>
+		<li data-search=""></li>
+		<li data-search=""></li>
+	</ul>
+</body>
+</html>
