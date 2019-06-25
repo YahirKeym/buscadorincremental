10a7a1b836d10fd4fa1e6cf1931fa0cea23f960e
YahirKeym <arzkaner@gmail.com>
2019-06-25 13:09:29 -0500

    Depuración 2 del buscador

--- assets/js/buscador.js
+++ assets/js/buscador.js
@@ -1,92 +1,72 @@
 /**
  * buscadorIncremental Hará la busqueda de elementos que le digamos, con los parametros y nodos que indiquemos
  * @author Yahir Axel Garcia Keymurth 20-Jun-2019
+ * @param  {String} cClaseBusquedaCorrecta   Es la clase que se usara para una busqueda correcta
+ * @param  {String} cClaseBusquedaIncorrecta Es la clase que se usara para los elementos que no coincidan con la busqueda 
  */
-function buscadorIncremental(cFirtsClass = "",cSecondClass ="") {
+function buscadorIncremental(cClaseBusquedaCorrecta = "",cClaseBusquedaIncorrecta ="") {
     /**
-     * _self Guardara el objeto de forma global
-     * @type {object}
-     */
-    _self = this;
-    /**
-     * cFirtsClass Será la clase que se afectuara a los campos seleccionados
+     * cClaseBusquedaCorrecta Será la clase que se afectuara a los campos seleccionados
      * @type {String}
      */
-    cFirtsClass = cFirtsClass
+    this.cClaseBusquedaCorrecta = cClaseBusquedaCorrecta
     /**
-     * cSecondClass Será la clase que se afectuara cuando los campos nos sean seleccionados
+     * cClaseBusquedaIncorrecta Será la clase que se afectuara cuando los campos nos sean seleccionados
      * @type {String}
      */
-    cSecondClass = cSecondClass
+    this.cClaseBusquedaIncorrecta = cClaseBusquedaIncorrecta
     /**
      * aParametros Guardara los parametros de busqueda
      * @type {Array}
      */
-    aParametros = {};
-    /**
-     * iElemento Nos dirá en que elemento nos encotramos, empezando por 0
-     * @type {Number}
-     */
-    iElemento = -1;
-    /**
-     * iBloquea description]
-     * @type {Number}
-     */
-    iBloquea = 0;
-    /**
-     * forEach Será la función forEach para hacer la repetición de elementos
-     * @type {Function}
-     */
-    forEach = Array.prototype.forEach;
+    this.cParametro = "";
     /**
      * cBusqueda Será el elemento en este caso input donde se escribirá la busqueda
      * @type {String}
      */
-    cBusqueda = "";
+    this.cBusqueda = "";
     /**
      * cCampoValor Serán los elementos en donde se hará la busqueda
      * @type {String}
      */
-    cCampoValor = "";
+    this.cCampoValor = "";
     /**
      * cValorLimpio Será los elementos sin Acentos
      * @type {String}
      */
-    cValorLimpio = "";
+    this.cValorLimpio = "";
     /**
-     * busca Ajustara la configuración del buscador
-     * @param  {String} cBuscador será el nombre del input que le traera los parametros
+     *  busca Ajustara la configuración del buscador
+     * @param  {string} nameInput             Es el nombre del input al que se le aplicara la busqueda
+     * @param  {strign} nameParametroBusqueda Es el nombre de los lugares donde se buscara
      */
-    this.busca = function(cBuscador = "", cParametro = "") {
-        aParametros['parametro'] = cParametro;
-        cBusqueda = document.querySelector(`input[name='${cBuscador}'`);
-        cCampoValor = document.querySelectorAll(`[${aParametros['parametro']}]`);
-        cBusqueda.addEventListener("keyup", this.keysUp);
+    this.busca = function(nameInput, nameParametroBusqueda) {
+        this.cBusqueda = document.querySelector(`input[name='${nameInput}'`);
+        this.cParametro = nameParametroBusqueda;
+        this.cBusqueda.addEventListener("keyup", this.keysUp);
     }
     /**
      * keysUp Serán las acciones después de pulsar x tecla en el input
      */
-    this.keysUp = function() {
-        var cValor = this.value;
-        cValorLimpio = _self.quitaAcentos(cValor);
-        iBloquea = 0;
-        iElemento++;
-        forEach.call(cCampoValor, _self.muestraBuqueda);
+    this.keysUp = (event) => {
+        this.cCampoValor = document.querySelectorAll(`[search-name='${this.cParametro}'] > [data-search]`);
+        var cValor = event.target.value;
+        this.cValorLimpio = this.quitaAcentos(cValor);
+        this.cCampoValor.forEach(this.muestraBuqueda);
     }
     /**
      * muestraBuqueda Ayudara a ocultar y mostrar los elementos mediante la busqueda
      * @param  {elementos} elements      Son los elementos que se encuentrar mediante la iteración
      * @param  {number} iCountElement Es el valor númerico del elemento
      */
-    this.muestraBuqueda = function(elements, iCountElement) {
-        var cValorSearch = elements.getAttribute(aParametros['parametro'])
-        var hiddenElement = document.querySelector(`[${aParametros['parametro']}='${cValorSearch}']`)
-        if (_self.quitaAcentos(elements.innerHTML.toLowerCase()).search(cValorLimpio.toLowerCase()) == -1) {
-            hiddenElement.classList.remove(cFirtsClass);
-            hiddenElement.classList.add(cSecondClass);
+    this.muestraBuqueda = (elements, iCountElement) => {
+        var cValorSearch = elements.getAttribute('data-search')
+        if (this.quitaAcentos(cValorSearch.toLowerCase()).search(this.cValorLimpio.toLowerCase()) == -1) {
+            elements.classList.remove(cClaseBusquedaCorrecta);
+            elements.classList.add(cClaseBusquedaIncorrecta);
         } else {
-            hiddenElement.classList.remove(cSecondClass);
-            hiddenElement.classList.add(cFirtsClass);
+            elements.classList.remove(cClaseBusquedaIncorrecta);
+            elements.classList.add(cClaseBusquedaCorrecta);
         }
     }
     /**
